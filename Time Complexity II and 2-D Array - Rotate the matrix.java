75276b4504f8b4c0ddd9fecbb607af2b68ba8d56
Vasu Patel <patelvasu1997@gmail.com>
2020-07-09 21:46:03 +0530

    2D Array programms added

---
+++ Time Complexity II and 2-D Array - Rotate the matrix.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Main{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int matrixSize = sc.nextInt();
+        int matrix[][] = new int[matrixSize][matrixSize];
+        for(int i = 0; i < matrixSize; i++){
+            for(int j = 0; j < matrixSize; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        // 90 degree rotate
+        for(int k = 0; k < matrixSize; k++){
+            for(int m = (matrixSize-1); m >= 0; m--){
+                System.out.print(matrix[m][k] + " ");
+            }   
+            System.out.println();
+        }
+        System.out.println();
+        // 180 degree rotate
+        for(int c = (matrixSize-1); c >= 0 ; c--){
+            for(int n = (matrixSize-1); n >= 0; n--){
+                System.out.print(matrix[c][n] + " ");
+            }   
+            System.out.println();
+        }
+        
+    }
+}
---
+++ Time Complexity II and 2-D Array - Simple-Transpose.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int matrixSize = sc.nextInt();
+        int matrix[][] = new int[matrixSize][matrixSize];
+        for(int i = 0; i < matrixSize; i++){
+            for(int j = 0; j < matrixSize; j++){
+                matrix[i][j] = sc.nextInt();
+            //    System.out.print(matrix[i][j]);
+            }
+        }
+        for(int k = 0; k < matrixSize; k++){
+            for(int l = 0; l < matrixSize; l++){
+                System.out.print(matrix[l][k] +" ");
+            }
+            System.out.println();
+        }
+    }
+}
